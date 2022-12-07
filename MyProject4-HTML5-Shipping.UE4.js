dd589be5fe8de1bf75a9b5dce7c68629f13ca0eb
ArkKapone <119900138+ArkKapone@users.noreply.github.com>
2022-12-07 22:48:17 +0100

    Update MyProject4-HTML5-Shipping.UE4.js

--- MyProject4-HTML5-Shipping.UE4.js
+++ MyProject4-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/ArkKapone/ue4mygame/5e07cc6480484517d527754d9ed85b1766bc7276/MyProject4-HTML5-Shipping.data.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
