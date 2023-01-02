384d4713a7ff5ecbc7a74a7a30dde798838c56c5
ArkKapone <119900138+ArkKapone@users.noreply.github.com>
2023-01-02 19:13:39 +0100

    Update MyProject4try-HTML5-Shipping.data.js

--- MyProject4try-HTML5-Shipping.data.js
+++ MyProject4try-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'G:/Games/Àíðèàë ýíæn gjgsnrf 2/MyProject4try/Binaries/HTML5/MyProject4try-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject4try-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/9l7hss9etpncrzr/MyProject4try-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/9l7hss9etpncrzr/MyProject4try-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
@@ -149,10 +149,10 @@ Module['FS_createPath']('/MyProject4try/Content', 'Paks', true, true);
           for (var i = 0; i < files.length; ++i) {
             DataRequest.prototype.requests[files[i].filename].onload();
           }
-              Module['removeRunDependency']('datafile_G:/Games/Àíðèàë ýíæn gjgsnrf 2/MyProject4try/Binaries/HTML5/MyProject4try-HTML5-Shipping.data');
+              Module['removeRunDependency']('datafile_G:/Games/ç†ç—‚åš¯ î‘–éŽ› gjgsnrf 2/MyProject4try/Binaries/HTML5/MyProject4try-HTML5-Shipping.data');
 
     };
-    Module['addRunDependency']('datafile_G:/Games/Àíðèàë ýíæn gjgsnrf 2/MyProject4try/Binaries/HTML5/MyProject4try-HTML5-Shipping.data');
+    Module['addRunDependency']('datafile_G:/Games/ç†ç—‚åš¯ î‘–éŽ› gjgsnrf 2/MyProject4try/Binaries/HTML5/MyProject4try-HTML5-Shipping.data');
   
     if (!Module.preloadResults) Module.preloadResults = {};
   
